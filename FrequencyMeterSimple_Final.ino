5e91e643de388c4c9c58a18b6a88a0ca4d483023
Sarah Cartwright <sarah@untamed.zone>
2023-12-05 19:47:33 +0000

    Update FrequencyMeterSimple_Final.ino

--- FrequencyMeterSimple_Final.ino
+++ FrequencyMeterSimple_Final.ino
@@ -1,4 +1,11 @@
-
+/*
+   Frequency counter and display.
+   
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+  
 #include <Arduino.h>
 
 #define freqInputPin GPIO_NUM_7
