9cfeb89dc89605f29a64f565f2a5edb24fb2ac4e
Sarah Cartwright <sarah@untamed.zone>
2023-12-05 19:46:34 +0000

    Update FrequencyMeter3.ino

--- FrequencyMeter3.ino
+++ FrequencyMeter3.ino
@@ -1,10 +1,11 @@
-/* Interrupt Example
+/* Frequency counter and display.
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   This code is in the Public Domain (or CC0 licensed, at your option.)
 
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
+   
 */
 
 #include <Arduino.h>
